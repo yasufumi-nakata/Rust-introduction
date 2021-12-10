5c7f5866ac83478cd8bf432e6753695c4c1f8884
yasufumi <52214705+yasufumi-nakata@users.noreply.github.com>
2021-12-10 23:14:56 +0900

    2021/12/10

--- rpn/src/main.rs
+++ rpn/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let exp = "6.1 5.2 4.3 * + 3.4 2.5 / 1.6 * -";
     let ans = rpn(exp);
-    debug_assert_eq!("26/2840", format!("{:.4}", ans));
+    debug_assert_eq!("26.2840", format!("{:.4}", ans));
     println!("{} = {:.4}", exp, ans);
 }
 fn rpn(exp: &str) -> f64 {
@@ -13,7 +13,7 @@ fn rpn(exp: &str) -> f64 {
             match token {
                 "+" => apply2(&mut stack, |x, y| x + y),
                 "-" => apply2(&mut stack, |x, y| x - y),
-                "*" => apply2(&mut stack, |x, y| x + y),
+                "*" => apply2(&mut stack, |x, y| x * y),
                 "/" => apply2(&mut stack, |x, y| x / y),
                 _ => panic!("Unknown operator : {}", token),
             }
